5a149ed4020938f84140829eebe1404a7d81facf
oscar.chen <2972789494@qq.com>
2021-06-23 13:58:38 +0800

    完成读取与写入的命令

--- cmd/read.go
+++ cmd/read.go
@@ -2,8 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
+
+	"github.com/airbike233/cftool/cftool"
 )
 
 func init() {
@@ -16,7 +21,53 @@ var readCmd = &cobra.Command{
 	Short: "write from cftool file",
 	Long: `Example:
 cftool r -f config.ini session.key`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("read called")
-	},
+	Run: readFile,
+}
+
+func readFile(_ *cobra.Command, args []string) {
+	if 1 > len(args) {
+		Exit("args error")
+	}
+	keys := strings.Split(args[0], ".")
+	data, err := ioutil.ReadFile(cfgFile)
+	if err != nil {
+		Exit(err)
+	}
+
+	switch FileType(cfgFile) {
+	case "ini":
+		i, err := cftool.NewJsonEdit(data)
+		if err == nil {
+			_, _ = fmt.Fprint(os.Stdout, i.GetValue(keys))
+		} else {
+			Exit(err)
+		}
+	case "json":
+		j, err := cftool.NewJsonEdit(data)
+		if err == nil {
+			_, _ = fmt.Fprint(os.Stdout, j.GetValue(keys))
+		} else {
+			Exit(err)
+		}
+	}
+	return
+}
+
+// FileType 返回文件后缀
+func FileType(path string) string {
+	s := strings.Split(path, ".")
+	if len(s) >= 2 {
+		return s[len(s)-1]
+	}
+	return ""
+}
+
+func Exitf(format string, a ...interface{}) {
+	_, _ = fmt.Fprintf(os.Stderr, format, a...)
+	os.Exit(1)
+}
+
+func Exit(des interface{}) {
+	_, _ = fmt.Fprint(os.Stderr, des)
+	os.Exit(1)
 }
--- cmd/write.go
+++ cmd/write.go
@@ -2,6 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/airbike233/cftool/cftool"
+	"io/ioutil"
+	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -15,8 +19,42 @@ var writeCmd = &cobra.Command{
 	Use:   "w",
 	Short: "write to cftool file",
 	Long: `Example:
-cftool r -f config.ini session.key`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("write called")
-	},
+cftool w -f config.ini session.key=value > config.ini`,
+	Run: writeFile,
+}
+
+func writeFile(_ *cobra.Command, args []string) {
+	if 1 > len(args) {
+		Exit("args error")
+	}
+	kv := strings.Split(args[0], "=")
+	if 2 > len(kv) {
+		Exit("args error")
+	}
+	value := kv[1]
+	keys := strings.Split(kv[0], ".")
+	data, err := ioutil.ReadFile(cfgFile)
+	if err != nil {
+		Exit(err)
+	}
+
+	switch FileType(cfgFile) {
+	case "ini":
+		i, err := cftool.NewJsonEdit(data)
+		if err == nil {
+			_ = i.SetValue(keys, value)
+			_, _ = fmt.Fprint(os.Stdout, i)
+		} else {
+			Exit(err)
+		}
+	case "json":
+		j, err := cftool.NewJsonEdit(data)
+		if err == nil {
+			_ = j.SetValue(keys, value)
+			_, _ = fmt.Fprint(os.Stdout, j)
+		} else {
+			Exit(err)
+		}
+	}
+	return
 }
